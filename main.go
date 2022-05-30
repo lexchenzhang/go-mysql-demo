f5b2873a440f7ac5628e755bfdc5e0341682044d
Lex <lexzh@outlook.com>
2022-05-30 14:00:24 -0500

    add model rep serv control route layers for posts

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"go-mysql/api/controller"
+	"go-mysql/api/repository"
+	"go-mysql/api/service"
 	"go-mysql/infrastructure"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"go-mysql/models"
+	"go-mysql/routes"
 )
 
 func main() {
+	router := infrastructure.NewGinRouter()                     //router has been initialized and configured
+	db := infrastructure.NewDatabase()                          // databse has been initialized and configured
+	postRepository := repository.NewPostRepository(db)          // repository are being setup
+	postService := service.NewPostService(postRepository)       // service are being setup
+	postController := controller.NewPostController(postService) // controller are being set up
+	postRoute := routes.NewPostRoute(postController, router)    // post routes are initialized
+	postRoute.Setup()                                           // post routes are being setup
 
-	router := gin.Default()
-
-	router.GET("/", func(context *gin.Context) {
-		infrastructure.NewDatabase()
-		context.JSON(http.StatusOK, gin.H{"data": "Hello World !"})
-	})
-	router.Run(":8000")
+	db.DB.AutoMigrate(&models.Post{}) // migrating Post model to datbase table
+	router.Gin.Run(":8000")           //server started on 8000 port
 }
