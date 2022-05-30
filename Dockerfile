e8fc8587e0fbeaf04da1782809509ab426459528
Lex <lexzh@outlook.com>
2022-05-30 15:09:19 -0500

    add user related model and logic hash pwd verify

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.13-buster AS builder
+FROM golang:1.18-buster AS builder
 WORKDIR /app
 
 COPY go.mod .
--- main.go
+++ main.go
@@ -18,6 +18,12 @@ func main() {
 	postRoute := routes.NewPostRoute(postController, router)    // post routes are initialized
 	postRoute.Setup()                                           // post routes are being setup
 
-	db.DB.AutoMigrate(&models.Post{}) // migrating Post model to datbase table
-	router.Gin.Run(":8000")           //server started on 8000 port
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepository)
+	userController := controller.NewUserController(userService)
+	userRoute := routes.NewUserRoute(userController, router)
+	userRoute.Setup()
+
+	db.DB.AutoMigrate(&models.Post{}, &models.User{})
+	router.Gin.Run(":8000") //server started on 8000 port
 }
